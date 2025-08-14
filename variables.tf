6ba84f50200154c0f000d1cc81849294cbf430b6
Karan Anpat <121686830+karananpat10@users.noreply.github.com>
2025-08-14 13:33:10 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,31 +1,5 @@
 
 # Declare the AWS region
-variable "region" {
-  default = "us-east-1"
-}
-
-# Declare the bucket name
-variable "bucket_name_prefix" {
-  default = "data20031203"
-}
-
-# Declare the Glue job name
-variable "glue_job_name" {
-  default = "yelpsdataglue12"
-}
-
-# Declare the Glue crawler name
-variable "glue_crawler_name" {
-  default = "yelpdatacrawler2023"
-}
-
-# Declare the script path in S3
-variable "script_s3_path" {
-  default = "s3://data20031203/scripts/glue_job.py"
-}
-
-=======
-# Declare the AWS region
 variable "region" {           # AWS region for the resources
   default = "us-east-1"       # default region
 }
